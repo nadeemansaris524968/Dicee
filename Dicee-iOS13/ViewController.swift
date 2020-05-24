001ec3ffda720baf502ef20fb71b1196bd386b2d
Nadeem Ansari <nadeem_ansari64@outlook.com>
2020-05-24 17:02:09 -0400

    Add code to randomize dice when roll button pressed.

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -11,11 +11,19 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var diceImageViewOne: UIImageView!
     @IBOutlet weak var diceImageViewTwo: UIImageView!
+    @IBOutlet weak var rollBtn: UIButton!
+    
+    let diceImages = [#imageLiteral(resourceName: "DiceOne"),#imageLiteral(resourceName: "DiceTwo"),#imageLiteral(resourceName: "DiceThree"),#imageLiteral(resourceName: "DiceFour"),#imageLiteral(resourceName: "DiceFive"),#imageLiteral(resourceName: "DiceSix")]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        diceImageViewOne.image = #imageLiteral(resourceName: "DiceThree")
-        diceImageViewTwo.image = #imageLiteral(resourceName: "DiceFour")
+        rollBtn.layer.cornerRadius = 5
+        rollBtn.layer.borderWidth = 0.5
+        rollBtn.layer.borderColor = UIColor.black.cgColor
     }
 
+    @IBAction func rollBtnPressed(_ sender: UIButton) {
+        diceImageViewOne.image = diceImages[Int.random(in: 0...5)]
+        diceImageViewTwo.image = diceImages[Int.random(in: 0...5)]
+    }
 }
