40015cd9508366f8797960f6577f02723e5da05e
Nadeem Ansari <nadeem_ansari64@outlook.com>
2020-05-24 15:22:04 -0400

    Add code to change image of both dices on view load.

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,12 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var diceImageViewOne: UIImageView!
+    @IBOutlet weak var diceImageViewTwo: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        diceImageViewOne.image = #imageLiteral(resourceName: "DiceThree")
+        diceImageViewTwo.image = #imageLiteral(resourceName: "DiceFour")
     }
 
-
 }
-
